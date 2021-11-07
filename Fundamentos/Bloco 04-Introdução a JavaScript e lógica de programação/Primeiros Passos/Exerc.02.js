44e43dd0b92802bbf89f72c340d5edbc0174cf91
Paula Francisca da Silva Mousquer de Castro <pauladecastro@gmail.com>
2021-11-06 23:45:42 -0300

    Maior de três

---
+++ Fundamentos/Bloco 04-Introdução a JavaScript e lógica de programação/Primeiros Passos/Exerc.02.js
@@ -0,0 +1,11 @@
+
+//Retorna o maior de dois números
+
+const compare1 = 47;  
+const compare2 = 85;
+
+if (compare1>compare2){
+    console.log(compare1)
+}
+else
+console.log(compare2);
---
+++ Fundamentos/Bloco 04-Introdução a JavaScript e lógica de programação/Primeiros Passos/Exerc.03.js
@@ -0,0 +1,13 @@
+const primeira = 78;
+const segunda = 86;
+const terceira = 18;
+
+if (primeira > segunda && primeira > segunda && primeira > terceira){
+    console.log(primeira);
+}
+else if(segunda > primeira && segunda > terceira){
+    console.log(segunda);
+}
+else if(terceira > primeira && terceira > segunda){
+    console.log(terceira)
+}
