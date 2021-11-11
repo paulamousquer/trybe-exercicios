2caf9b87c63afce7a17d4b4f6123d18ce65dc0f2
Paula Francisca da Silva Mousquer de Castro <pauladecastro@gmail.com>
2021-11-11 10:15:47 -0300

    Transformando nota em conceito

---
+++ Fundamentos/Bloco 04-Introdução a JavaScript e lógica de programação/Primeiros Passos/Exerc.07.js
@@ -0,0 +1,23 @@
+const  nota = 88;
+
+if( nota < 0 || nota > 100){
+    console.log("Erro, nota errada.")
+}
+else if (nota >=90){
+    console.log("A");
+}
+else if(nota >= 80){
+    console.log("B");
+}
+else if(nota >= 70){
+    console.log("C");
+}
+else if(nota >= 60){
+    console.log("D");
+}
+else if(nota >= 50){
+    console.log("D");
+}
+else if( nota > 0 || nota < 50){
+    console.log("F");
+}
